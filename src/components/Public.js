f67fb47d160c77ba3f8499b2c0d7f010d46cb49b
Ashan Millewa <ashanmillewa@gmail.com>
2023-12-20 10:18:26 +0530

    updated

--- src/components/Public.js
+++ src/components/Public.js
@@ -5,15 +5,14 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to Repair Store!</h1>
+                <h1>Welcome DTS</h1>
             </header>
             <main>
-                <p>Located in Beautiful Downtown Foo City, Repair Store provides a trained staff ready to meet your repair needs.</p>
+                <p></p>
                 <p>&nbsp;</p>
                 <address>
-                    Repair Store<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
+                    DTS<br />
+                    Colombo<br />
                     <a href="tel:+15555555555">(555) 555-5555</a>
                 </address>
             </main>
